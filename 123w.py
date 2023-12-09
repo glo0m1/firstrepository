fb1a4c79ac14b680c93cd0006d1d2b1f37084700
glo0m1 <153370236+glo0m1@users.noreply.github.com>
2023-12-09 17:01:40 +0300

    Add files via upload

---
+++ 123w.py
@@ -0,0 +1,23 @@
+def check_string_brackets(input_string):
+    a = input_string.count(')')
+    c = input_string.count('(')
+    if a == c:
+        print('YES')
+    else:
+        print('NO')
+b = input()
+check_string_brackets(b)
+
+
+
+
+
+
+
+
+
+
+
+
+
+
